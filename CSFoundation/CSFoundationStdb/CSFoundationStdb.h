566696c501f8d963ac5043da772c6b4eda6aff00
ChristianAndersen <595510982@qq.com>
2019-08-29 19:37:53 +0800

    添加工程

---
+++ CSFoundation/CSFoundationStdb/CSFoundationStdb.h
@@ -0,0 +1,13 @@
+//
+//  CSFoundationStdb.h
+//  CSFoundationStdb
+//
+//  Created by Andersen on 2019/8/29.
+//  Copyright © 2019 Andersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSFoundationStdb : NSObject
++(NSString*)version;
+@end
---
+++ CSFoundation/CSFoundationStdb/CSFoundationStdb.m
@@ -0,0 +1,16 @@
+//
+//  CSFoundationStdb.m
+//  CSFoundationStdb
+//
+//  Created by Andersen on 2019/8/29.
+//  Copyright © 2019 Andersen. All rights reserved.
+//
+
+#import "CSFoundationStdb.h"
+
+@implementation CSFoundationStdb
++(NSString*)version
+{
+    return @"static labrary version 1.0";
+}
+@end
