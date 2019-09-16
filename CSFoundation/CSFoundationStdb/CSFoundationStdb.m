939b57786c482f1dcf1a36c4a365ab779178408d
ChristianAndersen <1476756749@qq.com>
2019-09-16 17:55:09 +0800

    cocopods 添加标记

--- CSFoundation/CSFoundationStdb/CSFoundationStdb.m
+++ CSFoundation/CSFoundationStdb/CSFoundationStdb.m
@@ -17,6 +17,7 @@
 +(NSString*)meassageInfo
 {
     NSString *msg = @"AndersenLi add log";
+    msg = [msg stringByAppendingString:@"cocopods add"];
     return [NSString stringWithFormat:@"static labrary meassage %@",msg];
 }
 @end
